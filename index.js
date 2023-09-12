20f1502e041ff5254eddba66768d293ec41d97ae
Rahul Kumar <93612455+Rahulrockss@users.noreply.github.com>
2023-09-12 18:19:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let accordian = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+var typed = new Typed(".anima",{
+  strings:["Death Note","SuperMan","Joker","Avenger"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+})
